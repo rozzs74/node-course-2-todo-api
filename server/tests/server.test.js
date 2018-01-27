13857552e81701131d52fd9a85067f2e93d468d4
Royce <rozzs74@gmail.com>
2018-01-27 17:55:20 +0800

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,5 +98,44 @@ describe('GET /todos/:id', () => {
             .get('/todos/123abc')
             .expect(404)
             .end(done);
-    })
+    });
+
+    describe('DELETE /todos/:id', () => {
+        it('should remove todo document', (done) => {
+            var hexID = todos[1]._id.toHexString();
+
+            request(app)
+                .delete(`/todos/${hexID}`)
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todo._id).toBe(hexID);
+                })
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    Todo.findById(hexID).then((todo) => {
+                        expect(todo).toNotExist();
+                        done();
+                    }).catch((err) => done(err));
+                });
+        });
+
+        it('should return 404 if todo not found', (done) => {
+            var hexID = new ObjectID().toHexString();
+
+            request(app)
+                .delete(`/todos/${hexID}`)
+                .expect(404)
+                .end(done);
+        });
+
+        it('should return 404 if object is invalid', (done) => {
+            request(app)
+                .delete(`/todos/12312`)
+                .expect(404)
+                .end(done);
+        });
+    });
 });
