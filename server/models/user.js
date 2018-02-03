84d4a3afeb79ff9650ac362c7019e77e4c6cfae5
Royce <rozzs74@gmail.com>
2018-02-03 16:29:50 +0800

    Hashed user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,76 +2,95 @@ var mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const pick = require('lodash/pick');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
 
 // store schema for user
 var UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 1,
-        unique: true, //this field must not be equal that is why unique
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email'
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    tokens: [{
-        access: {
-            type: String,
-            required: true
-        },
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+	email: {
+		type: String,
+		require: true,
+		trim: true,
+		minlength: 1,
+		unique: true, //this field must not be equal that is why unique
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 }, {
-        usePushEach: true
-    });
+		usePushEach: true
+	});
 
 // Instance methods
 UserSchema.methods.toJSON = function () {
-    var user = this;
-    var userObject = user.toObject();
+	var user = this;
+	var userObject = user.toObject();
 
-    return pick(userObject, ['_id', 'email']);
+	return pick(userObject, ['_id', 'email']);
 }
 
 UserSchema.methods.generateAuthToken = function () {
-    var user = this;
-    var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
-    user.tokens.push({
-        access,
-        token
-    });
-    return user.save().then(() => {
-        return token;
-    });
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+	user.tokens.push({
+		access,
+		token
+	});
+	return user.save().then(() => {
+		return token;
+	});
 };
 
 UserSchema.statics.findByToken = function (token) {
-    var User = this;
-    var decoded;
+	var User = this;
+	var decoded;
 
-    try {
-        decoded = jwt.verify(token, 'abc123');
-    } catch (e) {
-        return Promise.reject();
-    }
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
 
-    return User.findOne({
-        _id: decoded._id,
-        'tokens.token': token,
-        'tokens.access': 'auth'
-    });
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
 };
+
+// Middleware
+UserSchema.pre('save', function (next) {
+
+	var user = this;
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next(); // call this to complete middleware event
+			});
+		});
+	} else {
+		next(); // call this to complete middleware event
+	}
+});
 
 var User = mongoose.model('User', UserSchema);
 
