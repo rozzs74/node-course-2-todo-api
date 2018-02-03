6b48b91851a09a2388b9ee9de86b6352f88d8cc3
Royce <rozzs74@gmail.com>
2018-02-03 18:56:40 +0800

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -280,25 +280,46 @@ describe("GET /todos/:id", () => {
 
 		it('should reject invalid login', (done) => {
 			request(app)
-			.post('/users/login')
-			.send({
-				email: users[1].email,
-				password: users[1].password + '1'
-			})
-			.expect(400)
-			.expect((res) => {
-				expect(res.headers['x-auth']).toNotExist();
-			})
-			.end((err, res) => {
-				if (err) {
-					return done(err);
-				}
+				.post('/users/login')
+				.send({
+					email: users[1].email,
+					password: users[1].password + '1'
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.headers['x-auth']).toNotExist();
+				})
+				.end((err, res) => {
+					if (err) {
+						return done(err);
+					}
 
-				User.findById(users[1]._id).then((user) => {
-					expect(user.tokens.length).toBe(0);
-					done();
-				}).catch((e) => done(e));
-			});
+					User.findById(users[1]._id).then((user) => {
+						expect(user.tokens.length).toBe(0);
+						done();
+					}).catch((e) => done(e));
+				});
+		});
+	});
+
+	describe('DELETE /users/me/token', () => {
+
+		it('should delete remove auth token on logout', (done) => {
+			request(app)
+				.delete('/users/me/token')
+				.set('x-auth', users[0].tokens[0].token)
+				.expect(400)
+				.end((err, res) => {
+					if (err) {
+						return done(err);
+					}
+					User.findById(users[0]._id).then((user) => {
+						expect(user.tokens.length).toBe(0);
+						done();
+					}).catch((e) => {
+						done(e);
+					});
+				});
 		});
 	});
 });
